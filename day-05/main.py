5571e4d9d85fb5a305d248ef76c59572762b8cd9
handicca <handikasyam.dev@gmail.com>
2024-10-18 18:38:53 +0700

    day 5 for loop & project PyPassword

---
+++ day-05/main.py
@@ -0,0 +1,16 @@
+# for loop list
+fruits = ["Appels", "Banana", "Orange"]
+
+for fruit in fruits:
+    print(f"I love {fruit}")
+
+# for loop with range()
+"""Ini akan me-return 0 - 9 tidak termasuk 10"""
+for number in range(10):
+    print(number)
+
+total = 0
+for num in range(1, 101):
+    total += num
+
+print(total)
