f2a5cbdc05cf00030e117cf2c026ceefdbc421bf
handicca <handikasyam.dev@gmail.com>
2024-10-19 10:43:16 +0700

    Function & while loop

---
+++ day-06/main.py
@@ -0,0 +1,22 @@
+# Function
+def my_function():
+    print("This function made by Me")
+
+# Invoke/calling function
+my_function()
+
+# While loop
+""" Syntax
+while something_is_true:
+    # do this
+"""
+
+count: int = 0
+while count < 5:
+    print(count)
+    count += 1
+
+"""
+Dalam contoh di atas, loop while akan terus berjalan selama count kurang dari 5.
+Setiap iterasi, nilai count dinaikkan satu.
+"""
