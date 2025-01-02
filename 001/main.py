966660f42b6cd06b64551ef13b10e713a2f9bcc1
handicca <handikasyam.dev@gmail.com>
2025-01-02 20:22:18 +0700

    Day 1

---
+++ 001/main.py
@@ -0,0 +1,27 @@
+# Day 1 Python Printing
+print("Hello World!")
+
+# String Manipulation and Code Intelligence
+print("Hello World!\nHello World!\nHello World!")
+# String Concatenation
+print("Hello" + " " + "World")
+print("Hello" + " World")
+
+# The Python Input Function
+print("Hello " + input("What is Your name? ") + "!")
+
+# Python Varibale
+name = "Handika"
+print(name)
+
+name = "Ridho"
+print(name)
+
+name = input("What is Your name? ")
+length = len(name)
+print(length)
+
+user_name = "@handicca"
+player_1 = "King JSHT"
+# 2player = "PHP" # Error
+_alamat = "Jl. Sigma"
