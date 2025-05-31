68e4326ab9fcf96ff50f2c1e9ebfc52426897536
Handika Syam <handikasyam.dev@gmail.com>
2025-05-31 18:10:48 +0700

    Day 27

---
+++ 027/argument.py
@@ -0,0 +1,58 @@
+"""Arguments with Default Values
+def my_function(a=1, b=2, c=3):
+    # Do this with a
+    # Then do this with b
+    # Finally do this wich c
+
+my_function()
+my_function(b=5)
+"""
+
+
+# *args: many psitional arguments; Unlimited Positional Arguments; type args is tuple
+# *args: Positional Variable-Length Arguments
+# * : unpacking operator
+def add(*nums):
+    sum = 0
+    for n in nums:
+        sum += n
+
+    return sum
+
+
+result = add(1, 2, 3, 4, 5)
+print(result)
+
+
+def display_name(*args):
+    for arg in args:
+        print(arg, end=" ")
+
+
+display_name("Prof.", "Dr.", "Ridho", "Syam")
+
+
+# **kwargs: Keyworded Variable-Length Arguments; type: **kwargs dict
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs.get("add")
+    n *= kwargs.get("multiply")
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+# How to use a **kwargs dictionary safely
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+my_car = Car(make="Nissan", model="Skyline")
+print(my_car.model)
---
+++ 027/main.py
@@ -0,0 +1,43 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(500, 300)
+window.config(padx=20, pady=20)
+
+"""
+pack()
+place(x=0, y=0)
+grid(cloumn=0, row=0)
+"""
+
+# Label
+my_label = tkinter.Label(text="New Label", font=("Arial", 24, "bold"))
+my_label.grid(column=0, row=0)
+
+# Change Label text
+my_label["text"] = "Prof. Dr. Ridho Syam"
+my_label.config(text="Rawwrrr")
+
+
+# Button
+def btn_click():
+    # print("I got Clicked")
+    # my_label["text"] = "Button got clicked"
+    my_label.config(text=form.get())
+
+
+btn1 = tkinter.Button(
+    text="Click Me",
+    command=btn_click,
+)
+btn1.grid(column=1, row=1)
+
+btn2 = tkinter.Button(text="New Button", command=btn_click)
+btn2.grid(column=2, row=0)
+
+# Entry
+form = tkinter.Entry(width=25)
+form.grid(column=3, row=2)
+
+window.mainloop()
